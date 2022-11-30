cef013441904392786275bf3deb340fa0dac8e92
Oleksandr Nedashkivskyi <77000134+nedashkivskyios@users.noreply.github.com>
2022-11-30 22:21:57 +0200

    chakra base setup (#7)

--- src/components/Unknown/App/index.tsx
+++ src/components/Unknown/App/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Root from "../Root";
+import {BrowserRouter} from "react-router-dom";
+import {ChakraProvider} from "@chakra-ui/react";
+import chakraTheme from "../../../common/chakraTheme";
 
 function App() {
     return (
-        <Root />
+        <BrowserRouter>
+            <ChakraProvider theme={chakraTheme}>
+                <Root/>
+            </ChakraProvider>
+        </BrowserRouter>
     );
 }
 
