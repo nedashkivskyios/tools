0123dbac1da0d962fd7fb6381a6ef15ee12e21da
Oleksandr Nedashkivskyi <77000134+nedashkivskyios@users.noreply.github.com>
2022-11-30 22:33:14 +0200

    basic setup eslint && prettier (#8)

--- src/components/Unknown/Root/index.tsx
+++ src/components/Unknown/Root/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
 import TemporaryMain from "../TemporaryMain";
 
 const Root = () => {
-    return (
-        <Routes>
-            <Route path="*" element={<TemporaryMain />}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="*" element={<TemporaryMain />} />
+    </Routes>
+  );
 };
 
 export default Root;
