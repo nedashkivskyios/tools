f539ed28e70295c2d92d60de20b6ee5d24996700
Oleksandr Nedashkivskyi <77000134+nedashkivskyios@users.noreply.github.com>
2022-12-02 12:42:29 +0200

    done (#11)

---
+++ src/common/firebaseConfig.ts
@@ -0,0 +1,10 @@
+const firebaseConfig =  {
+    apiKey: "AIzaSyAIO4JAZR9TT_cn3kIwWuGUFmZkTZaBxqk",
+    authDomain: "tools-b1227.firebaseapp.com",
+    projectId: "tools-b1227",
+    storageBucket: "tools-b1227.appspot.com",
+    messagingSenderId: "931525912489",
+    appId: "1:931525912489:web:0ccf88ca124f89975086b8"
+};
+
+export default firebaseConfig;
