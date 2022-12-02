e752da90b9f13c76ca1e89142a95cca9d7648d58
Oleksandr Nedashkivskyi <77000134+nedashkivskyios@users.noreply.github.com>
2022-12-02 13:05:56 +0200

    Implement react query

--- src/components/Unknown/App/index.tsx
+++ src/components/Unknown/App/index.tsx
@@ -1,17 +1,24 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import chakraTheme from "../../../common/chakraTheme";
 import Root from "../Root";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <ChakraProvider theme={chakraTheme}>
-        <Root />
-      </ChakraProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ChakraProvider theme={chakraTheme}>
+          <Root />
+        </ChakraProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" panelPosition="right" />
+    </QueryClientProvider>
   );
 }
 
